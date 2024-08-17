418beb491888bdd15013ee79c0beefcc6d2dec7b
esmeepeters <esmeepeters38@gmail.com>
2024-08-17 16:12:10 +0200

    added API for the tamperature
    also workt om the h1. If you type AMERSFOORT it wil say Amersfoort

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#app-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b7be44o389tb973131c1a0565c1f67ad";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeatherInfo);
+}
+
 function SearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElemant = document.querySelector("#search-form");
